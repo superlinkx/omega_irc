5367df5775db3940a1aff9b20a41446697f89e30
Steven Holms <superlinkx@gmail.com>
2020-05-16 04:43:31 -0600

    Initial commit

---
+++ example/omega_irc_example.dart
@@ -0,0 +1,6 @@
+import 'package:omega_irc/omega_irc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/omega_irc.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library omega_irc;
+
+export 'src/omega_irc_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
